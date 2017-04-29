d017e11dba88de7f31c75d2d6023378dcc003480
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2017-04-29 12:03:40 +0300

    Semantic URL

--- index.php
+++ index.php
@@ -8,8 +8,27 @@ function __autoload($className) {
 $route = $_GET['r'];
 $routeData = explode('/', $route);
 
+if(empty($routeData[0])) {
+    $routeData[0] = 'home';
+}
+if(empty($routeData[1])) {
+    $routeData[1] = 'index';
+}
+
 $controllerName = '\core\controllers\\' . ucfirst($routeData[0]) . 'Controller';
 $actionName = 'action' . ucfirst($routeData[1]);
+$path = substr(str_replace('\\', DIRECTORY_SEPARATOR, $controllerName) . '.php', 1);
 
-$controller = new $controllerName;
-echo $controller->$actionName();
+if(file_exists($path)) {
+    $controller = new $controllerName;
+
+    if (method_exists($controller, $actionName)) {
+        echo $controller->$actionName();
+    } else {
+        header('HTTP/1.1 404 Not Found');
+        echo 'Page not found.';
+    }
+} else {
+    header('HTTP/1.1 404 Not Found');
+    echo 'Page not found.';
+}
