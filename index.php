98b990a6230b82be95d7d2806b029edd26110f5e
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2017-04-29 11:24:57 +0300

    Rewrite

--- index.php
+++ index.php
@@ -5,3 +5,11 @@ function __autoload($className) {
     require_once $path;
 }
 
+$route = $_GET['r'];
+$routeData = explode('/', $route);
+
+$controllerName = $routeData[0];
+$actionName = $routeData[1];
+
+var_dump($_GET);
+var_dump($routeData);
